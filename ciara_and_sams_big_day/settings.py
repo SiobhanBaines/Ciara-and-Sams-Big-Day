c69fd146f750848eb9e0050300f6113b0b1276c7
SiobhanBaines <siobhan.baines@gmail.com>
2021-06-20 13:40:27 +0000

    set postgress config to os.envronment

--- ciara_and_sams_big_day/settings.py
+++ ciara_and_sams_big_day/settings.py
@@ -116,9 +116,11 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-
-# else:
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
