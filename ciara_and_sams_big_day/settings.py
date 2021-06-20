b59f524e62a8a7e29615756fd439a3fb604e8084
SiobhanBaines <siobhan.baines@gmail.com>
2021-06-20 13:49:10 +0000

    Remove secret key and set debug

--- ciara_and_sams_big_day/settings.py
+++ ciara_and_sams_big_day/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j74i7)qf30lxuu*pej&voig$n(wr3&lt**(gt=+v82$h6%nqgh'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ciara-and-sams-wedding.herokuapp.com', 'localhost']
 
