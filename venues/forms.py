a5929faf8570c1e510dfd5715fed662e0c79b0c9
SiobhanBaines <siobhan.baines@gmail.com>
2021-07-06 20:00:21 +0000

    Add google maps to venues

--- venues/forms.py
+++ venues/forms.py
@@ -17,5 +17,32 @@ class VenueForm(forms.ModelForm):
             'email',
             'phone_number',
             'venue_url',
+            'lat',
+            'lng',
             'image',
         )
+
+    def __init__(self, *args, **kwargs):
+            """
+            Add placeholders and classes, remove auto-generated
+            labels and set autofocus on first field
+            """
+            super().__init__(*args, **kwargs)
+            placeholders = {
+                'venue_type': 'Ceremony/Recpetion',
+                'name': 'Venue Name',
+                'address_line_1': 'Address Line',
+                'address_line_2': 'Address Line',
+                'city': 'City',
+                'county': 'County',
+                'postcode': 'Post Code',
+                'email': 'Contact Email Address',
+                'phone_number': 'Telephone Number',
+                'venue_url': 'Venue Wedsite',
+                'lat': 'Location Latitude',
+                'lng': 'Location Longditude',
+            }
+
+            for field in self.fields:
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].label = False
--- venues/models.py
+++ venues/models.py
@@ -13,6 +13,8 @@ class Venue(models.Model):
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     venue_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    lat = models.FloatField(null=True, blank=True)
+    lng = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.venue_type
